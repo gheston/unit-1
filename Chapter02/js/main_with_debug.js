3dc5cf20fa8e44bb2811fb93259c491a1696fe6f
Gerald Heston <gerald@hestongis.com>
2023-02-05 10:02:57 -0800

    debugging main_with_debugs.js
    Activity 3

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -1,3 +1,8 @@
+// Geog 575 Activity 3
+// Gerald Heston
+// Feb 5 2023
+
+//Creates an array cityPop with 4 elements, each with 2 objects .city and .population
 var cityPop = [
 	{ 
 		city: 'Madison',
@@ -17,31 +22,92 @@ var cityPop = [
 	}
 ];
 
+// log the array cityPop - yes it's being created correctly
+// console.log(cityPop);
+
+
+// functions initialize() and cities() copied from from example 3.4 - otherwise there was no table for addColumns or addEvents to do anything to
+
+//initialize function called when the script loads
+function initialize() {
+	cities();
+};
+
+
+function cities() {
+
+	//create the table element
+	var table = document.createElement("table");
+
+	//create a header row
+	var headerRow = document.createElement("tr");
+
+	//add the "City" and "Population" columns to the header row
+	headerRow.insertAdjacentHTML("beforeend", "<th>City</th><th>Population</th>")
+
+	//add the row to the table
+	table.appendChild(headerRow);
+
+	//loop to add a new row for each city
+	for (var i = 0; i < cityPop.length; i++) {
+		//assign longer html strings to a variable
+		var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+		//add the row's html string to the table
+		table.insertAdjacentHTML('beforeend', rowHtml);
+	}
+
+	document.querySelector("#mydiv").appendChild(table);
+};
+
+//added this function to initialize function addColumns() called when the script loads
+function initializeAddColumns() {
+	addColumns(cityPop); //had to pass cityPop to the function otherwise the variables were undefined
+};
+
 function addColumns(cityPop){
     
     document.querySelectorAll("tr").forEach(function(row, i){
-
+		// i = 0; thought it might need an itorator but the .forEach doesn't need a separate iterator
+		//console.log(i);
+		//console.log(cityPop[i].population)
     	if (i == 0){
 
-    		row.insertAdjacntHTML('beforeend', '<th>City Size</th>');
+    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>'); //"insertAdjacntHTML should be insertAdjacentHTML
     	} else {
 
     		var citySize;
 
-    		if (cityPop[i-1].population < 100000){
+    		if (cityPop[i-1].population < 100000){  //by now i =1, so we have to subtract 1 to get back to 0
+				// console.log(i);
+				// console.log(cityPop[i-1].population);
     			citySize = 'Small';
+				// console.log(citySize);
 
     		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
-
+    			// console.log(i);
+				// console.log(cityPop[i-1].population);
+				citySize = 'Medium'; // "citysize should be "citySize"
+				// console.log(citySize);
     		} else {
-    			citySize = 'Large';
+    			// console.log(i);
+				// console.log(cityPop[i-1].population);
+				citySize = 'Large';
+				// console.log(citySize);
     		};
 
-			row.insertAdjacntHTML = '<td' + citySize + '</td>';
+			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>'); //"insertAdjacntHTML should be insertAdjacentHTML, also <td needed a closing >; also needed to be in the same format as the previous row.insertAdjacentHTML, in (), with 'beforeend' as the 1st argument
+			//console.log('<td>' + citySize + '</td>');
     	};
+		// i++; thought it might need an itorator but the .forEach doesn't need a separate iterator
     });
+
+
 };
+
+//added this to initialize addEvents() called when the script loads
+function initializeAddEvents() {
+	addEvents();
+}
 
 function addEvents(){
 
@@ -52,17 +118,22 @@ function addEvents(){
 		for (var i=0; i<3; i++){
 
 			var random = Math.round(Math.random() * 255);
+			//console.log(random);
 
-			color += "random";
+			color += random; // random shouldn't be a string
 
 			if (i<2){
 				color += ",";
 			
 			} else {
 				color += ")";
-		};
+			};
+		
+			//console.log(color);
 
-		document.querySelector("table").color = color;
+		} // needed a } to close the for loop on line 114
+
+		document.querySelector("table").style.color = color; // needed style.color
 	});
 
 	function clickme(){
@@ -72,3 +143,8 @@ function addEvents(){
 
 	document.querySelector("table").addEventListener("click", clickme)
 };
+
+//move this line to the end of the script - call the functions after the page DOM loads
+document.addEventListener('DOMContentLoaded', initialize);
+document.addEventListener('DOMContentLoaded', initializeAddColumns);
+document.addEventListener('DOMContentLoaded', initializeAddEvents);
